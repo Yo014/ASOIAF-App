4435d42a4c741c243885f465a23a90fe2df48533
Yo014 <132107622+Yo014@users.noreply.github.com>
2025-04-19 18:12:24 -0400

    all of arya chapters are filled in all the books

--- ASOIAF APP/Arya.py
+++ ASOIAF APP/Arya.py
@@ -252,99 +252,298 @@ class AryaBookII(ctk.CTkFrame):
 
 
 class AryaBookIII(ctk.CTkFrame):
-    def __init__(self,parent, controller):
+    """
+    A class to represent the GUI for Arya's chapters in Book III of ASOIAF.
+    """
+    def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
+
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("blue")
 
-        # Initialize GUI components
         self.image_label = ctk.CTkLabel(self, text="")
         self.image_label.place(x=130, y=10)
 
-        self.text_box = ctk.CTkTextbox(self,font=("Arial", 15), width=450, height=100, state="disabled")
+        self.text_box = ctk.CTkTextbox(self, font=("Arial", 15), width=450, height=100, state="disabled")
         self.text_box.place(x=50, y=405)
 
         self.create_slider()
         self.create_buttons()
-        
+
     def create_slider(self):
-        self.slider = ctk.CTkSlider(self, 
-        from_=1, to=10, number_of_steps=9,
-        command=self.slider_changed, 
-        width=400, 
-        progress_color="dark red",
-        button_color="white",
-        button_hover_color="white",
-        height=30
+        self.slider = ctk.CTkSlider(
+            self,
+            from_=1, to=13, number_of_steps=12,
+            command=self.slider_changed,
+            width=400,
+            progress_color="dark red",
+            button_color="white",
+            button_hover_color="white",
+            height=30
         )
         self.slider.place(x=75, y=530)
+        self.slider.set(1)
 
     def slider_changed(self, value):
         chapter_index = int(value)
         chapter_name = f"Arya {self.convert_to_roman(chapter_index)}"
-        self.display_Arya(chapter_name) 
+        self.display_Arya(chapter_name)
 
     def convert_to_roman(self, num):
-        roman_numerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII"]
+        roman_numerals = [
+            "I", "II", "III", "IV", "V", "VI", "VII",
+            "VIII", "IX", "X", "XI", "XII", "XIII"
+        ]
         return roman_numerals[num - 1]
 
     def create_buttons(self):
-        from ASOIAF_APP import MainMenuPage
+        from POV_Characters import POVCharactersBookIII
         button_back = ctk.CTkButton(
-            self, text="Back", command=lambda: self.controller.show_frame(MainMenuPage), width=80, height=30, 
-            corner_radius=8
+            self, text="Back", command=lambda: self.controller.show_frame(POVCharactersBookIII),
+            width=80, height=30, corner_radius=8,
+            bg_color="transparent", fg_color="transparent", hover_color="grey"
         )
         button_back.place(x=20, y=30)
+
     def show_Arya_Chapters(self):
         return {
             "Arya I": {
                 "image": "Arya_Stark.png",
-                "text": "Arya I: Arya travels north with Yoren and a bunch of recruits bound for the Wall."
-                },
+                "text": "Arya I: Arya, Gendry, and Hot Pie travel north from Harrenhal."
+            },
             "Arya II": {
                 "image": "Arya_Stark.png",
-                "text": "Arya II:Yoren's group is accosted by gold cloaks looking for Gendry."
-                },
+                "text": "Arya II: Arya, Gendry, and Hot Pie are captured by the Brotherhood Without Banners. Harwin recognizes her and reveals her identity."
+            },
             "Arya III": {
                 "image": "Arya_Stark.png",
-                "text": "Arya III:Yoren takes his party off the kingsroad in an effort to avoid pursuit "
-                },
+                "text": "Arya III: The group of outlaws escort Arya, Hot Pie, and Gendry through the Riverlands looking for Lord Beric Dondarrion."
+            },
             "Arya IV": {
                 "image": "Arya_Stark.png",
-                "text": "Arya IV:Yoren's party is attacked at an abandoned holdfast by Ser Amory Lorch. Arya escapes through a tunnel under the barn."
-                },    
+                "text": "Arya IV: Greenbeard and his men continue their search for Lord Beric."
+            },
             "Arya V": {
                 "image": "Arya_Stark.png",
-                "text": "Arya V:Arya, Gendry and Hot Pie are captured by Gregor Clegane."
-                },
+                "text": "Arya V: The outlaws come to Stoney Sept hunting for Lord Beric. The Mad Huntsman captures Sandor Clegane."
+            },
             "Arya VI": {
                 "image": "Arya_Stark.png",
-                "text": "Arya VI:Arya is brought to Harrenhal, where she is made a servant."
-                },
+                "text": "Arya VI: Arya and Sandor are brought before Lord Beric. Sandor kills Beric in a trial by combat, but Beric returns alive minutes later."
+            },
             "Arya VII": {
                 "image": "Arya_Stark.png",
-                "text": "Arya VII:Jaqen H'ghar promises to kill three men of Arya's choosing in return for saving his life."
-                },
+                "text": "Arya VII: Lord Beric ambushes a group of Brave Companions."
+            },
             "Arya VIII": {
                 "image": "Arya_Stark.png",
-                "text": "Arya VIII:Lord Tywin marches from Harrenhal. Arya has Jaqen kill Weese."
-                },
+                "text": "Arya VIII: Arya runs from the Brotherhood and gets captured by Sandor."
+            },
             "Arya IX": {
                 "image": "Arya_Stark.png",
-                "text": "Arya IX:Arya enlists Jaqen's help in rescuing a group of newly-arrived northmen captives, who it turns out allowed themselves to be captured on purpose in order to take the castle. Roose Bolton arrives and takes charge of Harrenhal."
-                },
+                "text": "Arya IX: Sandor takes Arya towards the Twins so he can ransom her to Robb."
+            },
             "Arya X": {
                 "image": "Arya_Stark.png",
-                "text": "Arya X:Arya, Gendry, and Hot Pie leave Harrenhal after Arya learns that Lord Roose means to give the castle to Vargo Hoat."},
+                "text": "Arya X: Sandor and Arya arrive at the Twins."
+            },
             "Arya XI": {
-                },
+                "image": "Arya_Stark.png",
+                "text": "Arya XI: Arya and Sandor approach the Twins as the slaughter begins. Sandor prevents Arya from running into the castle."
+            },
             "Arya XII": {
-                },
+                "image": "Arya_Stark.png",
+                "text": "Arya XII: Arya and Sandor travel towards the Eyrie, then Riverrun."
+            },
             "Arya XIII": {
-                },
+                "image": "Arya_Stark.png",
+                "text": "Arya XIII: Sandor fights with Gregor’s men and is wounded. Arya leaves him and takes passage with a Braavosi ship using Jaqen’s coin."
+            }
         }
-    
+
+    def display_Arya(self, Arya_name):
+        profiles = self.show_Arya_Chapters()
+        Arya_chapter = profiles[Arya_name]
+        chapter_text = f"{Arya_chapter['text']}"
+        self.update_text_box(chapter_text)
+        self.update_image(Arya_chapter['image'])
+
+    def update_text_box(self, text):
+        self.text_box.configure(state="normal")
+        self.text_box.delete("1.0", ctk.END)
+        self.text_box.insert(ctk.END, text)
+        self.text_box.configure(state="disabled")
+
+    def update_image(self, image_name):
+        try:
+            image_path = f"C:/Users/emuki/OneDrive/Desktop/ASOIAF APP/ASOIAF-App/ASOIAF APP/Png Files/{image_name}"
+            pil_image = Image.open(image_path).resize((300, 390))
+            ctk_image = ctk.CTkImage(pil_image, size=(300, 390))
+            self.image_label.configure(image=ctk_image)
+            self.image_label.image = ctk_image
+        except Exception as e:
+            print(f"Error loading image: {e}")
+            self.update_text_box("\n[Image could not be loaded.]")
+
+
+class AryaBookIV(ctk.CTkFrame):
+    """
+    A class to represent the GUI for Arya's chapters in Book IV of ASOIAF.
+    """
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("blue")
+
+        self.image_label = ctk.CTkLabel(self, text="")
+        self.image_label.place(x=130, y=10)
+
+        self.text_box = ctk.CTkTextbox(self, font=("Arial", 15), width=450, height=100, state="disabled")
+        self.text_box.place(x=50, y=405)
+
+        self.create_slider()
+        self.create_buttons()
+
+    def create_slider(self):
+        self.slider = ctk.CTkSlider(
+            self,
+            from_=1, to=3, number_of_steps=2,
+            command=self.slider_changed,
+            width=400,
+            progress_color="dark red",
+            button_color="white",
+            button_hover_color="white",
+            height=30
+        )
+        self.slider.place(x=75, y=530)
+        self.slider.set(1)
+
+    def slider_changed(self, value):
+        chapter_index = int(value)
+        chapter_name = self.convert_to_label(chapter_index)
+        self.display_Arya(chapter_name)
+
+    def convert_to_label(self, index):
+        labels = ["Arya I", "Arya II", "Cat Of The Canals"]
+        return labels[index - 1]
+
+    def create_buttons(self):
+        from POV_Characters import POVCharactersBookIV
+        button_back = ctk.CTkButton(
+            self, text="Back", command=lambda: self.controller.show_frame(POVCharactersBookIV),
+            width=80, height=30, corner_radius=8,
+            bg_color="transparent", fg_color="transparent", hover_color="grey"
+        )
+        button_back.place(x=20, y=30)
+
+    def show_Arya_Chapters(self):
+        return {
+            "Arya I": {
+                "image": "Arya_Stark.png",
+                "text": "Arya I: Arya arrives at Braavos and is accepted as a servant of the Many-Faced God."
+            },
+            "Arya II": {
+                "image": "Arya_Stark.png",
+                "text": "Arya II: Arya is accepted as an acolyte of the Many-Faced God and begins training. She learns Braavosi and is sent into the city to sell shellfish for Brusco."
+            },
+            "Cat Of The Canals": {
+                "image": "Arya_Stark.png",
+                "text": "Cat Of The Canals: Arya sells clams for Brusco and kills Dareon, a deserter. She tells the Kindly Man, who gives her milk, and she wakes up blind the next day."
+            }
+        }
+
+    def display_Arya(self, Arya_name):
+        profiles = self.show_Arya_Chapters()
+        Arya_chapter = profiles[Arya_name]
+        chapter_text = f"{Arya_chapter['text']}"
+        self.update_text_box(chapter_text)
+        self.update_image(Arya_chapter['image'])
+
+    def update_text_box(self, text):
+        self.text_box.configure(state="normal")
+        self.text_box.delete("1.0", ctk.END)
+        self.text_box.insert(ctk.END, text)
+        self.text_box.configure(state="disabled")
+
+    def update_image(self, image_name):
+        try:
+            image_path = f"C:/Users/emuki/OneDrive/Desktop/ASOIAF APP/ASOIAF-App/ASOIAF APP/Png Files/{image_name}"
+            pil_image = Image.open(image_path).resize((300, 390))
+            ctk_image = ctk.CTkImage(pil_image, size=(300, 390))
+            self.image_label.configure(image=ctk_image)
+            self.image_label.image = ctk_image
+        except Exception as e:
+            print(f"Error loading image: {e}")
+            self.update_text_box("\n[Image could not be loaded.]")
+
+
+class AryaBookV(ctk.CTkFrame):
+    """
+    A class to represent the GUI for Arya's chapters in Book V of ASOIAF.
+    """
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("blue")
+
+        self.image_label = ctk.CTkLabel(self, text="")
+        self.image_label.place(x=130, y=10)
+
+        self.text_box = ctk.CTkTextbox(self, font=("Arial", 15), width=450, height=100, state="disabled")
+        self.text_box.place(x=50, y=405)
+
+        self.create_slider()
+        self.create_buttons()
+
+    def create_slider(self):
+        self.slider = ctk.CTkSlider(
+            self,
+            from_=1, to=2, number_of_steps=1,
+            command=self.slider_changed,
+            width=400,
+            progress_color="dark red",
+            button_color="white",
+            button_hover_color="white",
+            height=30
+        )
+        self.slider.place(x=75, y=530)
+        self.slider.set(1)
+
+    def slider_changed(self, value):
+        chapter_index = int(value)
+        chapter_name = self.convert_to_label(chapter_index)
+        self.display_Arya(chapter_name)
+
+    def convert_to_label(self, index):
+        labels = ["The Blind Girl", "The Ugly Little Girl"]
+        return labels[index - 1]
+
+    def create_buttons(self):
+        from POV_Characters import POVCharactersBookV
+        button_back = ctk.CTkButton(
+            self, text="Back", command=lambda: self.controller.show_frame(POVCharactersBookV),
+            width=80, height=30, corner_radius=8,
+            bg_color="transparent", fg_color="transparent", hover_color="grey"
+        )
+        button_back.place(x=20, y=30)
+
+    def show_Arya_Chapters(self):
+        return {
+            "The Blind Girl": {
+                "image": "Arya_Stark.png",
+                "text": "The Blind Girl: Arya develops new skills while training blind at the House of White and Black."
+            },
+            "The Ugly Little Girl": {
+                "image": "Arya_Stark.png",
+                "text": "The Ugly Little Girl: Arya accepts an assassination assignment and is given a new face."
+            }
+        }
+
     def display_Arya(self, Arya_name):
         profiles = self.show_Arya_Chapters()
         Arya_chapter = profiles[Arya_name]
