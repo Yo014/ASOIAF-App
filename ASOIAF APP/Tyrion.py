a86ddc0e620cf38bbfcfac79d8943a4b843e3894
Yo014 <132107622+Yo014@users.noreply.github.com>
2025-04-19 20:25:05 -0400

    Update Tyrion.py

--- ASOIAF APP/Tyrion.py
+++ ASOIAF APP/Tyrion.py
@@ -45,49 +45,46 @@ class TyrionBookI(ctk.CTkFrame):
             corner_radius=8,bg_color="transparent",fg_color="transparent",hover_color="grey",font=("Arial", 18)
         )
         button_back.place(x=20, y=30)
-    
     def show_Tyrion_chapter(self):
         return {
-            "Tyrion I": {
-                "content": "Tyrion I content",
-                "image": "Tyrion_lannister.png"
-            },
-            "Tyrion II": {
-                "content": "Tyrion II content",
-                "image": "Tyrion_lannister.png"
-                
-            },
-            "Tyrion III": {
-                "content": "Tyrion III content",
-                "image": "Tyrion_lannister.png"
-            },
-            "Tyrion IV": {
-                "content": "Tyrion IV content",
-                "image": "Tyrion_lannister.png"
-            },
-            "Tyrion V": {
-                "content": "Tyrion V content",
-                "image": "Tyrion_lannister.png"
-            },
-            "Tyrion VI": {
-                "content": "Tyrion VI content",
-                "image": "Tyrion_lannister.png"
-            },
-            "Tyrion VII": {
-                "content": "Tyrion VII content",
-                "image": "Tyrion_lannister.png"
-            },
-            "Tyrion VIII": {
-                "content": "Tyrion VIII content",
-                "image": "Tyrion_lannister.png"
-            },
-            "Tyrion IX": {
-                "content": "Tyrion IX content",
-                "image": "Tyrion_lannister.png"
-            },
-
-          
+        "Tyrion I": {
+            "content": "Tyrion I:Tyrion has breakfast with Cersei and Jaime, noting that Bran lies crippled and in a coma, but unlikely to die from the fall. Tyrion announces his plans to see the Wall.",
+            "image": "Tyrion_lannister.png"
+        },
+        "Tyrion II": {
+            "content": "Tyrion II:En route to the Wall, Tyrion convinces Jon that life in the Night's Watch will be difficult.",
+            "image": "Tyrion_lannister.png"
+        },
+        "Tyrion III": {
+            "content": "Tyrion III:On his last night with the Night's Watch, Tyrion promises Lord Commander Mormont that he will inform his father and the King about the condition of the Wall and he promises Jon Snow to help Bran as he helped Jon.",
+            "image": "Tyrion_lannister.png"
+        },
+        "Tyrion IV": {
+            "content": "Tyrion IV:Catelyn Tully is bringing Tyrion to the Eyrie, the residence of her sister Lysa. Accompanying them on the dangerous path are various knights, sellswords and a singer. Tyrion finds a flaw in Littlefinger's story about the dagger.",
+            "image": "Tyrion_lannister.png"
+        },
+        "Tyrion V": {
+            "content": "Tyrion V:Tyrion is confined to a cell and not allowed to see anyone. He finally gets an audience with Lysa by promising to confess his crimes and demands a trial by combat.",
+            "image": "Tyrion_lannister.png"
+        },
+        "Tyrion VI": {
+            "content": "Tyrion VI:Tyrion tells Bronn about his marriage to Tysha. They are accosted by mountain clansmen to whom Tyrion promises aid in taking the Vale.",
+            "image": "Tyrion_lannister.png"
+        },
+        "Tyrion VII": {
+            "content": "Tyrion VII:Tyrion arrives at Lord Tywin's headquarters.",
+            "image": "Tyrion_lannister.png"
+        },
+        "Tyrion VIII": {
+            "content": "Tyrion VIII:Tyrion fights on the left flank in a battle against Roose Bolton.",
+            "image": "Tyrion_lannister.png"
+        },
+        "Tyrion IX": {
+            "content": "Tyrion IX:Tywin orders Tyrion to go to King's Landing and rule in his place.",
+            "image": "Tyrion_lannister.png"
         }
+    }
+    
     def display_Tyrion(self, tyrion_name):
         profiles = self.show_Tyrion_chapter()
         Tyrion_chapter = profiles[tyrion_name]
