c47de0d6914bdad6f2015275812b6a2c3c975dbd
Yo014 <132107622+Yo014@users.noreply.github.com>
2025-04-26 19:12:54 -0400

    Update Daenerys.py

--- ASOIAF APP/Daenerys.py
+++ ASOIAF APP/Daenerys.py
@@ -112,3 +112,105 @@ class DaenerysBookI(ctk.CTkFrame):
         except Exception as e:
             print(f"Error loading image: {e}")
             self.update_text_box("\n[Image could not be loaded.]")
+
+
+class DaenerysBookII(ctk.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        # Initialize GUI components
+        self.image_label = ctk.CTkLabel(self, text="")
+        self.image_label.place(x=130, y=10)
+
+        self.text_box = ctk.CTkTextbox(self, font=("Arial", 15), width=450, height=100, state="disabled")
+        self.text_box.place(x=50, y=405)
+
+        self.create_slider()
+        self.create_buttons()
+
+    def create_slider(self):
+        self.slider = ctk.CTkSlider(
+            self, 
+            from_=1, to=5, number_of_steps=4,
+            command=self.slider_changed, 
+            width=400, 
+            progress_color="purple",
+            button_color="white",
+            button_hover_color="white",
+            height=30
+        )
+        self.slider.set(1)
+        self.slider.place(x=75, y=530)
+
+    def slider_changed(self, value):
+        chapter_index = int(value)
+        chapter_name = f"Daenerys {self.convert_to_roman(chapter_index)}"
+        self.display_Daenerys(chapter_name)
+
+    def convert_to_roman(self, num):
+        roman_numerals = ["I", "II", "III", "IV", "V"]
+        return roman_numerals[num-1]
+
+    def create_buttons(self):
+        from POV_Characters import POVCharactersBookII
+        button_back = ctk.CTkButton(
+            self, text="Back",
+            command=lambda: self.controller.show_frame(POVCharactersBookII),
+            width=80, height=30,
+            corner_radius=8,
+            bg_color="transparent",
+            fg_color="transparent",
+            hover_color="grey",
+            font=("Arial", 18)
+        )
+        button_back.place(x=20, y=30)
+
+    def show_Daenerys_chapter(self):
+        return {
+            "Daenerys I": {
+                "content": "Daenerys I: Daenerys and her small khalasar wander the Red Waste in search of a safe haven.",
+                "image": "Daenerys_Targaryen.png"
+            },
+            "Daenerys II": {
+                "content": "Daenerys II: Daenerys arrives at Qarth and is hosted by the merchant princes.",
+                "image": "Daenerys_Targaryen.png"
+            },
+            "Daenerys III": {
+                "content": "Daenerys III: Daenerys seeks allies and ships to conquer Westeros, but finds no support.",
+                "image": "Daenerys_Targaryen.png"
+            },
+            "Daenerys IV": {
+                "content": "Daenerys IV: Daenerys visits the House of the Undying and has strange visions.",
+                "image": "Daenerys_Targaryen.png"
+            },
+            "Daenerys V": {
+                "content": "Daenerys V: Xaro Xhoan Daxos offers Daenerys marriage and a ship, but she refuses. Her dragons are stolen.",
+                "image": "Daenerys_Targaryen.png"
+            }
+        }
+
+    def display_Daenerys(self, daenerys_name):
+        profiles = self.show_Daenerys_chapter()
+        Daenerys_chapter = profiles[daenerys_name]
+        chapter_text = f"{Daenerys_chapter['content']}"
+        self.update_text_box(chapter_text)
+        self.update_image(Daenerys_chapter['image'])
+
+    def update_text_box(self, text):
+        self.text_box.configure(state="normal")
+        self.text_box.delete("1.0", ctk.END)
+        self.text_box.insert(ctk.END, text)
+        self.text_box.configure(state="disabled")
+
+    def update_image(self, image_name):
+        try:
+            image_path = f"C:/Users/emuki/OneDrive/Desktop/ASOIAF APP/ASOIAF-App/ASOIAF APP/Png Files/{image_name}"
+            pil_image = Image.open(image_path).resize((300, 390))
+            ctk_image = ctk.CTkImage(pil_image, size=(300, 390))
+            self.image_label.configure(image=ctk_image)
+            self.image_label.image = ctk_image
+        except Exception as e:
+            print(f"Error loading image: {e}")
+            self.update_text_box("\n[Image could not be loaded.]")
+
