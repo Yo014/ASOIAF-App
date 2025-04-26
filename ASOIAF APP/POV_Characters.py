5c769b16b4c0d04d45b1fea35ac282dd327bda19
Yo014 <132107622+Yo014@users.noreply.github.com>
2025-04-26 19:35:01 -0400

    Update POV_Characters.py

--- ASOIAF APP/POV_Characters.py
+++ ASOIAF APP/POV_Characters.py
@@ -2,14 +2,16 @@ import customtkinter as ctk
 from PIL import Image
 from CTkScrollableDropdown import *
 from Arya import AryaBookI, AryaBookII
-from Bran import BranBookI
-from Catelyn import CatelynBookI
+from Bran import BranBookI, BranBookII
+from Catelyn import CatelynBookI, CatelynBookII
 from Eddard import EddardBookI
-from Jon import JonBookI
-from Book_Info import BookInfo_BookI
-from Sansa import SansaBookI
-from Tyrion import TyrionBookI
-from Daenerys import DaenerysBookI
+from Jon import JonBookI, JonBookII
+from Book_Info import BookInfo_BookI, BookInfo_BookII
+from Sansa import SansaBookI, SansaBookII
+from Tyrion import TyrionBookI, TyrionBookI
+from Daenerys import DaenerysBookI, DaenerysBookI
+from Davos import DavosBookII
+from Theon import TheonBookII
 
 class POVCharactersBookI(ctk.CTkFrame):
     """
@@ -100,73 +102,64 @@ class POVCharactersBookI(ctk.CTkFrame):
             width=80, height=30, corner_radius=8)
         button_back.place(x=20, y=30)       
 
-
+## book2
 
 class POVCharactersBookII(ctk.CTkFrame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
 
-        # Initialize GUI components
         self.image_label = ctk.CTkLabel(self, bg_color="light blue")
-        self.image_label.place(x=210, y=20)  # Adjust placement
+        self.image_label.place(x=210, y=20)
+
         image_path = "C:/Users/emuki/OneDrive/Desktop/ASOIAF APP/ASOIAF-App/ASOIAF APP/Png Files/A_Clash_of_Kings.png"
         image = Image.open(image_path)
-        resized_image = image.resize((150, 190))  # Resize the image
+        resized_image = image.resize((150, 190))
         photo = ctk.CTkImage(resized_image, size=(150, 190))
         self.image_label.configure(image=photo)
         self.image_label.image = photo
-        
-        # Add buttons to the interface
+
+        self.create_entry()
         self.create_buttons()
-        
+
+    def create_entry(self):
+        entry = ctk.CTkEntry(self, width=340)
+        entry.place(x=100, y=250)
+
+        def insert_method(e):
+            entry.delete(0, 'end')
+            entry.insert(0, e)
+
+            if e == "Davos":
+                self.controller.show_frame(DavosBookII)
+            elif e == "Theon":
+                self.controller.show_frame(TheonBookII)
+            elif e == "Arya":
+                self.controller.show_frame(AryaBookII)
+            elif e == "Bran":
+                self.controller.show_frame(BranBookII)
+            elif e == "Catelyn":
+                self.controller.show_frame(CatelynBookII)
+            elif e == "Sansa":
+                self.controller.show_frame(SansaBookII)
+            elif e == "Jon":
+                self.controller.show_frame(JonBookII)
+            elif e == "Daenerys":
+                self.controller.show_frame(DaenerysBookII)
+            elif e == "Tyrion":
+                self.controller.show_frame(TyrionBookII)
+
+        values = ["Davos", "Theon", "Arya", "Bran", "Catelyn", "Sansa", "Jon", "Daenerys", "Tyrion"]
+        CTkScrollableDropdown(entry, values=values, command=lambda e: insert_method(e), autocomplete=True, width=340)
+
     def create_buttons(self):
-        from Arya import AryaBookI, AryaBookII
         from ASOIAF_APP import MainMenuPage
-        # Add buttons to the interface
-        button_config = {
-            "Davos":{ "name": "Davos", "command": self.show_davos_Chapters, "position": (0, 250)},
-            "Catelyn":{ "name": "Catelyn", "command": self.show_catelyn_Chapters, "position": (282, 250)},
-            "Tyrion":{ "name": "Tyrion", "command": self.show_tyrion_Chapters, "position": (0, 337)},
-            "Jon":{ "name": "Jon", "command": self.show_Characters, "position": (282, 337)},
-            "Daenerys":{ "name": "Daenerys", "command": self.show_daenerys_Chapters, "position": (0, 425)},
-            "Arya":{ "name": "Arya", "command": lambda: self.controller.show_frame(AryaBookII), "position": (282, 425)},
-            "Sansa":{ "name": "Sansa", "command": self.show_sansa_Chapters, "position": (0, 513)},
-            "Bran":{ "name": "Bran", "command": self.show_bran_Chapters, "position": (187, 513)},
-            "Theon":{ "name": "Theon", "command": self.show_theon_Chapters, "position": (374, 513)},
-        }
-        button_back = ctk.CTkButton(self, text="Back", command=lambda: self.controller.show_frame(MainMenuPage), width=80, height=30, corner_radius=8)
+        button_back = ctk.CTkButton(
+            self, text="Back",
+            command=lambda: self.controller.show_frame(MainMenuPage),
+            width=80, height=30, corner_radius=8
+        )
         button_back.place(x=20, y=30)
-
-        for key, config in button_config.items():
-            button = ctk.CTkButton(
-                self,
-                text=config["name"],
-                command=config["command"],
-                corner_radius=8,
-                fg_color="white",
-                text_color="black",
-                hover_color="lightgray",
-                font=("Arial", 20)
-            )
-            button.place(x=config["position"][0], y=config["position"][1])
-
-    def show_davos_Chapters(self):
-        pass    
-    def show_catelyn_Chapters(self):
-        pass           
-    def show_tyrion_Chapters(self):
-        pass
-    def show_Characters(self):
-        pass
-    def show_daenerys_Chapters(self):
-        pass
-    def show_sansa_Chapters(self):
-        pass
-    def show_bran_Chapters(self):
-        pass
-    def show_theon_Chapters(self):
-        pass
     
 class POVCharactersBookIII(ctk.CTkFrame):
     def __init__(self, parent, controller):
